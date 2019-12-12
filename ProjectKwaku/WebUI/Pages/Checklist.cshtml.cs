3b47e3d078f7e4e51c0e5b4e8e329f3bcb62e70b
Michael Tse <tsem@emea.cshare.net>
2019-12-12 16:42:00 +0000

    Created checklist page and removed privacy page

---
+++ ProjectKwaku/WebUI/Pages/Checklist.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebUI.Pages
+{
+    public class TasksModel : PageModel
+    {
+        public string TaskCategory { get; set; }
+
+        public void OnGet(string taskCategory)
+        {
+            TaskCategory = taskCategory;
+        }
+    }
+}
