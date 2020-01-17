29bb16522b1bbf687fe3574cfffeba353ea086a3
michael <mtse41@gmail.com>
2020-01-17 22:31:53 +0000

    Resized logo,  hooked up nav bar with checklists from API, imported font awesome

--- ProjectKwaku/WebUI/ClientApp/src/components/navmenu/navmenu.js
+++ ProjectKwaku/WebUI/ClientApp/src/components/navmenu/navmenu.js
@@ -4,23 +4,59 @@ import './navmenu.css';
 export class NavMenu extends Component {
     static displayName = NavMenu.name;
 
-    render() {
-        return (
-			<div id="sidebar" className="brand-purple-bg">
-				<div id="sidebar-brand">
-					<div id="sidebar-brand-logo">
-						<a href="/">
-							<img className="logo" src="images/cpu-logo-white.png" alt="Computershare" />
-						</a>
-					</div>
+    constructor() {
+        super();
+        this.state = { checklistTypes: [], loading: true };
+    }
 
-					<div id="sidebar-tools">
-						<button id="sidebar-toggle">
-							<span>
-							</span>
-						</button>
-					</div>
-				</div>
+    componentDidMount() {
+        this.getChecklistTypes();
+    }
+
+    async getChecklistTypes() {
+        const response = await fetch('api/checklisttype/all');
+        const data = await response.json();
+        this.setState({ checklistTypes: data, loading: false });
+    }
+
+    getUrl(checklistTypeId) {
+        return "/checklist/" + checklistTypeId;
+    }
+
+    getMenuOptions(checklistTypes) {
+        return (
+            checklistTypes.map(checklistType =>
+                <li className="sidebar-menu-item checklist-item" key={checklistType.checklistTypeId}>
+                    <a className="sidebar-menu-link" href={this.getUrl(checklistType.checklistTypeId)}>
+                        <span className="sidebar-menu-text">{checklistType.name}</span>
+                    </a>
+                </li>
+            )
+        );
+    }
+
+    render() {
+        let menuOptions = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : this.getMenuOptions(this.state.checklistTypes);
+
+        return (
+            <div id="sidebar" className="brand-purple-bg">
+                <div id="sidebar-brand">
+                    <div id="sidebar-brand-logo">
+                        <a href="/">
+                            <img src="images/cpu-logo-white.png" alt="Computershare" />
+                        </a>
+                    </div>
+
+                    <div id="sidebar-tools">
+                        <button id="sidebar-toggle">
+                            <span>
+                                <i className="fas fa-chevron-left white"></i>
+                            </span>
+                        </button>
+                    </div>
+                </div>
 
                 <div id="menu-wrapper">
                     <div id="sidebar-menu">
@@ -40,40 +76,11 @@ export class NavMenu extends Component {
                             <li className="sidebar-menu-section checklist-item">
                                 <h4 className="sidebar-menu-section-text">Checklists</h4>
                             </li>
-                            <li className="sidebar-menu-item checklist-item">
-                                <a className="sidebar-menu-link" href="checklist.html">
-                                    <span className="sidebar-menu-text">North America</span>
-                                </a>
-                            </li>
-                            <li className="sidebar-menu-item checklist-item">
-                                <a className="sidebar-menu-link" href="#">
-                                    <span className="sidebar-menu-text">EMEA</span>
-                                </a>
-                            </li>
-                            <li className="sidebar-menu-item checklist-item">
-                                <a className="sidebar-menu-link" href="#">
-                                    <span className="sidebar-menu-text">Oceania</span>
-                                </a>
-                            </li>
-                            <li className="sidebar-menu-item checklist-item">
-                                <a className="sidebar-menu-link" href="#">
-                                    <span className="sidebar-menu-text">Miscellaneous</span>
-                                </a>
-                            </li>
-                            <li className="sidebar-menu-item checklist-item">
-                                <a className="sidebar-menu-link" href="#">
-                                    <span className="sidebar-menu-text">CLS</span>
-                                </a>
-                            </li>
-                            <li className="sidebar-menu-item checklist-item">
-                                <a className="sidebar-menu-link" href="#">
-                                    <span className="sidebar-menu-text">CVS</span>
-                                </a>
-                            </li>
+                            {menuOptions}
                         </ul>
                     </div>
                 </div>
-			</div>
+            </div>
         );
     }
 }
--- ProjectKwaku/WebUI/ClientApp/src/index.js
+++ ProjectKwaku/WebUI/ClientApp/src/index.js
@@ -1,4 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import '@fortawesome/fontawesome-free/css/fontawesome.css';
+import '@fortawesome/fontawesome-free/css/solid.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
