56a019351c9cf8763fae4889f830f0c4e492458d
Michael Tse <tsem@emea.cshare.net>
2020-01-17 16:02:18 +0000

    Integrating sidebar with React

--- ProjectKwaku/WebUI/ClientApp/src/components/navmenu/navmenu.js
+++ ProjectKwaku/WebUI/ClientApp/src/components/navmenu/navmenu.js
@@ -4,47 +4,76 @@ import './navmenu.css';
 export class NavMenu extends Component {
     static displayName = NavMenu.name;
 
-    constructor(props) {
-        super(props);
-
-        this.toggleNavbar = this.toggleNavbar.bind(this);
-        this.state = {
-            collapsed: true
-        };
-    }
-
-    toggleNavbar() {
-        this.setState({
-            collapsed: !this.state.collapsed
-        });
-    }
-
     render() {
         return (
-            <div className="vertical-nav bg-white" id="sidebar">
-                <p className="font-weight-bold px-3 small pb-4 mb-0">Checklists</p>
+			<div id="sidebar" className="brand-purple-bg">
+				<div id="sidebar-brand">
+					<div id="sidebar-brand-logo">
+						<a href="/">
+							<img className="logo" src="images/cpu-logo-white.png" alt="Computershare" />
+						</a>
+					</div>
 
-                <ul className="nav flex-column bg-white mb-0">
-                    <li className="nav-item">
-                        <a href="#" className="nav-link text-dark font-italic bg-light">
-                            <i className="fa fa-th-large mr-3 text-primary fa-fw"></i>
-                            <span>Home</span>
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#" className="nav-link text-dark font-italic">
-                            <i className="fa fa-address-card mr-3 text-primary fa-fw"></i>
-                            <span>About</span>
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#" className="nav-link text-dark font-italic">
-                            <i className="fa fa-cubes mr-3 text-primary fa-fw"></i>
-                            <span>Services</span>
-                        </a>
-                    </li>
-                </ul>
-            </div>
+					<div id="sidebar-tools">
+						<button id="sidebar-toggle">
+							<span>
+							</span>
+						</button>
+					</div>
+				</div>
+
+                <div id="menu-wrapper">
+                    <div id="sidebar-menu">
+                        <ul id="sidebar-menu-nav">
+                            <li className="sidebar-menu-item active">
+                                <a className="sidebar-menu-link" href="/">
+                                    <span className="sidebar-menu-icon"></span>
+                                    <span className="sidebar-menu-text">Dashboard</span>
+                                </a>
+                            </li>
+                            <li className="sidebar-menu-item">
+                                <a className="sidebar-menu-link" href="#">
+                                    <span className="sidebar-menu-icon"></span>
+                                    <span className="sidebar-menu-text">Settings</span>
+                                </a>
+                            </li>
+                            <li className="sidebar-menu-section checklist-item">
+                                <h4 className="sidebar-menu-section-text">Checklists</h4>
+                            </li>
+                            <li className="sidebar-menu-item checklist-item">
+                                <a className="sidebar-menu-link" href="checklist.html">
+                                    <span className="sidebar-menu-text">North America</span>
+                                </a>
+                            </li>
+                            <li className="sidebar-menu-item checklist-item">
+                                <a className="sidebar-menu-link" href="#">
+                                    <span className="sidebar-menu-text">EMEA</span>
+                                </a>
+                            </li>
+                            <li className="sidebar-menu-item checklist-item">
+                                <a className="sidebar-menu-link" href="#">
+                                    <span className="sidebar-menu-text">Oceania</span>
+                                </a>
+                            </li>
+                            <li className="sidebar-menu-item checklist-item">
+                                <a className="sidebar-menu-link" href="#">
+                                    <span className="sidebar-menu-text">Miscellaneous</span>
+                                </a>
+                            </li>
+                            <li className="sidebar-menu-item checklist-item">
+                                <a className="sidebar-menu-link" href="#">
+                                    <span className="sidebar-menu-text">CLS</span>
+                                </a>
+                            </li>
+                            <li className="sidebar-menu-item checklist-item">
+                                <a className="sidebar-menu-link" href="#">
+                                    <span className="sidebar-menu-text">CVS</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+			</div>
         );
     }
 }
