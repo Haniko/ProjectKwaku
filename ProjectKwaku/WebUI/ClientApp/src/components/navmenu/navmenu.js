8733c641a3ee41a32334121879b7a64e32c00483
Michael Tse <tsem@emea.cshare.net>
2020-01-23 15:32:54 +0000

    Integrating sidebar styling + states

--- ProjectKwaku/WebUI/ClientApp/src/components/navmenu/navmenu.js
+++ ProjectKwaku/WebUI/ClientApp/src/components/navmenu/navmenu.js
@@ -5,13 +5,22 @@ import './navmenu.css';
 export class NavMenu extends Component {
     static displayName = NavMenu.name;
 
-    constructor() {
-        super();
-        this.state = { checklistTypes: [], loading: true };
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            checklistTypes: [],
+            loading: true,
+            sidebarCollapsed: false
+        };
     }
 
     componentDidMount() {
         this.getChecklistTypes();
+    }
+
+    onSidebarToggle = () => {
+        this.setState({ sidebarCollapsed: !this.state.sidebarCollapsed });
     }
 
     async getChecklistTypes() {
@@ -28,9 +37,9 @@ export class NavMenu extends Component {
         return (
             checklistTypes.map(checklistType =>
                 <li className="sidebar-menu-item checklist-item" key={checklistType.checklistTypeId}>
-                    <Link className="sidebar-menu-link" to={this.getUrl(checklistType.checklistTypeId)}>
+                    <NavLink className="sidebar-menu-link" activeClassName="active" exact to={this.getUrl(checklistType.checklistTypeId)}>
                         <span className="sidebar-menu-text">{checklistType.name}</span>
-                    </Link>
+                    </NavLink>
                 </li>
             )
         );
@@ -41,8 +50,10 @@ export class NavMenu extends Component {
             ? <p><em>Loading...</em></p>
             : this.getMenuOptions(this.state.checklistTypes);
 
+        const sidebarCSS = this.state.sidebarCollapsed ? "sidebar-minimised" : "";
+
         return (
-            <div id="sidebar" className="brand-purple-bg">
+            <div id="sidebar" className={"brand-purple-bg " + sidebarCSS}>
                 <div id="sidebar-brand">
                     <div id="sidebar-brand-logo">
                         <Link to="/">
@@ -51,9 +62,15 @@ export class NavMenu extends Component {
                     </div>
 
                     <div id="sidebar-tools">
-                        <button id="sidebar-toggle">
+                        <button id="sidebar-toggle" onClick={this.onSidebarToggle}>
                             <span>
-                                <i className="fas fa-chevron-left white"></i>
+                                <svg id="toggle-icon" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                    <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                                        <polygon points="0 0 24 0 24 24 0 24"></polygon>
+                                        <path d="M5.29288961,6.70710318 C4.90236532,6.31657888 4.90236532,5.68341391 5.29288961,5.29288961 C5.68341391,4.90236532 6.31657888,4.90236532 6.70710318,5.29288961 L12.7071032,11.2928896 C13.0856821,11.6714686 13.0989277,12.281055 12.7371505,12.675721 L7.23715054,18.675721 C6.86395813,19.08284 6.23139076,19.1103429 5.82427177,18.7371505 C5.41715278,18.3639581 5.38964985,17.7313908 5.76284226,17.3242718 L10.6158586,12.0300721 L5.29288961,6.70710318 Z" fill="#ffffff" fillRule="nonzero" transform="translate(8.999997, 11.999999) scale(-1, 1) translate(-8.999997, -11.999999) "></path>
+                                        <path d="M10.7071009,15.7071068 C10.3165766,16.0976311 9.68341162,16.0976311 9.29288733,15.7071068 C8.90236304,15.3165825 8.90236304,14.6834175 9.29288733,14.2928932 L15.2928873,8.29289322 C15.6714663,7.91431428 16.2810527,7.90106866 16.6757187,8.26284586 L22.6757187,13.7628459 C23.0828377,14.1360383 23.1103407,14.7686056 22.7371482,15.1757246 C22.3639558,15.5828436 21.7313885,15.6103465 21.3242695,15.2371541 L16.0300699,10.3841378 L10.7071009,15.7071068 Z" fill="#ffffff" fillRule="nonzero" opacity="0.3" transform="translate(15.999997, 11.999999) scale(-1, 1) rotate(-270.000000) translate(-15.999997, -11.999999) "></path>
+                                    </g>
+                                </svg>
                             </span>
                         </button>
                     </div>
@@ -63,20 +80,40 @@ export class NavMenu extends Component {
                     <div id="sidebar-menu">
                         <ul id="sidebar-menu-nav">
                             <li className="sidebar-menu-item">
-                                <NavLink className="sidebar-menu-link" activeClassName="active" to="/">
-                                    <span className="sidebar-menu-icon"></span>
+                                <NavLink className="sidebar-menu-link" activeClassName="active" exact to="/">
+                                    <span className="sidebar-menu-icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1">
+                                            <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                                                <polygon points="0 0 24 0 24 24 0 24"></polygon>
+                                                <path d="M12.9336061,16.072447 L19.36,10.9564761 L19.5181585,10.8312381 C20.1676248,10.3169571 20.2772143,9.3735535 19.7629333,8.72408713 C19.6917232,8.63415859 19.6104327,8.55269514 19.5206557,8.48129411 L12.9336854,3.24257445 C12.3871201,2.80788259 11.6128799,2.80788259 11.0663146,3.24257445 L4.47482784,8.48488609 C3.82645598,9.00054628 3.71887192,9.94418071 4.23453211,10.5925526 C4.30500305,10.6811601 4.38527899,10.7615046 4.47382636,10.8320511 L4.63,10.9564761 L11.0659024,16.0730648 C11.6126744,16.5077525 12.3871218,16.5074963 12.9336061,16.072447 Z" fill="#ffffff" fillRule="nonzero"></path>
+                                                <path d="M11.0563554,18.6706981 L5.33593024,14.122919 C4.94553994,13.8125559 4.37746707,13.8774308 4.06710397,14.2678211 C4.06471678,14.2708238 4.06234874,14.2738418 4.06,14.2768747 L4.06,14.2768747 C3.75257288,14.6738539 3.82516916,15.244888 4.22214834,15.5523151 C4.22358765,15.5534297 4.2250303,15.55454 4.22647627,15.555646 L11.0872776,20.8031356 C11.6250734,21.2144692 12.371757,21.2145375 12.909628,20.8033023 L19.7677785,15.559828 C20.1693192,15.2528257 20.2459576,14.6784381 19.9389553,14.2768974 C19.9376429,14.2751809 19.9363245,14.2734691 19.935,14.2717619 L19.935,14.2717619 C19.6266937,13.8743807 19.0546209,13.8021712 18.6572397,14.1104775 C18.654352,14.112718 18.6514778,14.1149757 18.6486172,14.1172508 L12.9235044,18.6705218 C12.377022,19.1051477 11.6029199,19.1052208 11.0563554,18.6706981 Z" fill="#ffffff" opacity="0.3"></path>
+                                            </g>
+                                        </svg>
+                                    </span>
+
                                     <span className="sidebar-menu-text">Dashboard</span>
                                 </NavLink>
                             </li>
+
                             <li className="sidebar-menu-item">
-                                <NavLink className="sidebar-menu-link" activeClassName="active" to="/counter">
-                                    <span className="sidebar-menu-icon"></span>
+                                <NavLink className="sidebar-menu-link" activeClassName="active" exact to="/counter">
+                                    <span className="sidebar-menu-icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1">
+                                            <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                                                <rect x="0" y="0" width="24" height="24" />
+                                                <path d="M5,8.6862915 L5,5 L8.6862915,5 L11.5857864,2.10050506 L14.4852814,5 L19,5 L19,9.51471863 L21.4852814,12 L19,14.4852814 L19,19 L14.4852814,19 L11.5857864,21.8994949 L8.6862915,19 L5,19 L5,15.3137085 L1.6862915,12 L5,8.6862915 Z M12,15 C13.6568542,15 15,13.6568542 15,12 C15,10.3431458 13.6568542,9 12,9 C10.3431458,9 9,10.3431458 9,12 C9,13.6568542 10.3431458,15 12,15 Z" fill="#ffffff" />
+                                            </g>
+                                        </svg>
+                                    </span>
+
                                     <span className="sidebar-menu-text">Settings</span>
                                 </NavLink>
                             </li>
+
                             <li className="sidebar-menu-section checklist-item">
                                 <h4 className="sidebar-menu-section-text">Checklists</h4>
                             </li>
+
                             {menuOptions}
                         </ul>
                     </div>
