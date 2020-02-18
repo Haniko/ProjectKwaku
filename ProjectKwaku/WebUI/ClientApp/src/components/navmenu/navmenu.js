0a36caff0fadde93a4f5f90105286a60cd486ac1
Liam Aitken <aitkenl@emea.cshare.net>
2020-02-18 15:04:57 +0000

    Included new reporting menu option in the sidebar

--- ProjectKwaku/WebUI/ClientApp/src/components/navmenu/navmenu.js
+++ ProjectKwaku/WebUI/ClientApp/src/components/navmenu/navmenu.js
@@ -92,12 +92,12 @@ export class NavMenu extends Component {
                                         <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1">
                                             <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
                                                 <rect x="0" y="0" width="24" height="24" />
-                                                <path d="M5,8.6862915 L5,5 L8.6862915,5 L11.5857864,2.10050506 L14.4852814,5 L19,5 L19,9.51471863 L21.4852814,12 L19,14.4852814 L19,19 L14.4852814,19 L11.5857864,21.8994949 L8.6862915,19 L5,19 L5,15.3137085 L1.6862915,12 L5,8.6862915 Z M12,15 C13.6568542,15 15,13.6568542 15,12 C15,10.3431458 13.6568542,9 12,9 C10.3431458,9 9,10.3431458 9,12 C9,13.6568542 10.3431458,15 12,15 Z" fill="#ffffff" />
+                                                <path d="M14,16 L12,16 L12,12.5 C12,11.6715729 11.3284271,11 10.5,11 C9.67157288,11 9,11.6715729 9,12.5 L9,17.5 C9,19.4329966 10.5670034,21 12.5,21 C14.4329966,21 16,19.4329966 16,17.5 L16,7.5 C16,5.56700338 14.4329966,4 12.5,4 L12,4 C10.3431458,4 9,5.34314575 9,7 L7,7 C7,4.23857625 9.23857625,2 12,2 L12.5,2 C15.5375661,2 18,4.46243388 18,7.5 L18,17.5 C18,20.5375661 15.5375661,23 12.5,23 C9.46243388,23 7,20.5375661 7,17.5 L7,12.5 C7,10.5670034 8.56700338,9 10.5,9 C12.4329966,9 14,10.5670034 14,12.5 L14,16 Z" fill="#ffffff" fill-rule="nonzero" transform="translate(12.500000, 12.500000) rotate(-315.000000) translate(-12.500000, -12.500000) " />
                                             </g>
                                         </svg>
                                     </span>
 
-                                    <span className="sidebar-menu-text d-flex align-items-center text-white">Settings</span>
+                                    <span className="sidebar-menu-text d-flex align-items-center text-white">Reporting</span>
                                 </NavLink>
                             </li>
 
