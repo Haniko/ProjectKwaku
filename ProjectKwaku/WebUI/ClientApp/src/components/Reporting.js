29dada1a38ec0e1c628fd5ddb8dc6ed0d41430f0
Liam Aitken <aitkenl@emea.cshare.net>
2020-03-02 17:09:06 +0000

    Added dropdown list of Checksheet Types to the Reporting page.

--- ProjectKwaku/WebUI/ClientApp/src/components/Reporting.js
+++ ProjectKwaku/WebUI/ClientApp/src/components/Reporting.js
@@ -4,20 +4,52 @@ export class Reporting extends Component {
     static displayName = Reporting.name;
 
   constructor(props) {
-    super(props);
-  }
+      super(props);
 
-  render() {
-      return (
+      this.state = {
+          checkSheetTypes: [],
+          loading: true
+      };
+    }
 
+    componentDidMount() {
+        this.getCheckSheetTypes();
+    }
+
+    async getCheckSheetTypes() {
+        const response = await fetch('api/checksheets/types');
+        const data = await response.json();
+        this.setState({ checkSheetTypes: data, loading: false });
+    }
+
+    renderSelect(checkSheetTypes) {
+        return (
+            <select>
+                {
+                    checkSheetTypes.map(checkSheetType =>
+                        <option key={checkSheetType.checkSheetTypeId}>
+                            {checkSheetType.name}
+                        </option>
+                    )
+                }
+            </select>
+        );
+    }
+
+    render() {
+
+        var content = (this.state.loading) ? "<h1>Loading...</h1>" : this.renderSelect(this.state.checkSheetTypes);
+
+        return (
           <>
-
               <div id="sub-header" className="d-flex flex-row align-items-center justify-content-between py-0 px-4 border-bottom bg-white">
                   <div>
                       <h4 id="sub-header-title" className="m-0 p-0">Reporting</h4>
                   </div>
               </div>
-        </>
-    );
-  }
+
+              {content}
+          </>
+        );
+    }
 }
